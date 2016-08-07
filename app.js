a11e7e30d176a720afc6fd429fa82f598049089d
Admin <admin@Admins-MacBook-Pro.local>
2016-08-07 10:30:27 -0700

    fixed bugs

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 	// Initial array of movies
 	var animals = ['Cat', 'Tiger', 'Puppy', 'Lion'];
 
@@ -20,11 +21,11 @@
                     var p = $('<p>').text("Rating: " + results[i].rating);
 
                     var animalImage = $('<img>');
-                    animalImage.addClass("animalImage");
                     animalImage.attr('src', results[i].images.fixed_height_still.url);
-                    animalImage.attr('data-state', 'still');
                     animalImage.attr('data-still', results[i].images.fixed_height_still.url);
                     animalImage.attr('data-animate', results[i].images.fixed_height.url);
+                    animalImage.attr('data-state', 'still');
+                    animalImage.addClass("animalImage");
                     animalDiv.append(p);
                     animalDiv.append(animalImage);
                     $('#animalsView').prepend(animalDiv);
@@ -57,19 +58,17 @@
 		    $('#buttonsView').append(a); // Added the button to the HTML
 		}
 	}
-
 	// ========================================================
-
 	// This function handles events where one button is clicked
 	$('#addAnimal').on('click', function(){
 
 		// This line of code will grab the input from the textbox
 		var animal = $('#animal-input').val().trim();
 
-		// The movie from the textbox is then added to our array
+		// The animal from the textbox is then added to our array
 		animals.push(animal);
 		
-		// Our array then runs which handles the processing of our movie array
+		// Our array then runs which handles the processing of our animal array
 		renderButtons();
 
 		// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
@@ -85,9 +84,26 @@
 	renderButtons();
 
 
-$(".animalImage").click(function(){
-	var state = $(this).attr('data-state'); 
-	        
+
+	$('body').on('click', ".animalImage" ,function(){
+	    	//STEP ONE: study the html above. Look at all the data attributes. Run the file in the browser. Look at the images. After you fill in steps 1 and 2 you'll be able to pause gifs from giphy.
+
+	    	//STEP TWO: make a variable named state and then reference the button's data-state into it. Do not use .data('state'). It won't work the way we expect.
+
+	    	//---------------FILL IN CODE HERE FOR STEP TWO----------------------------
+	        var state = $(this).attr('data-state'); 
+	        //----------------------------------------------------
+
+        	/*STEP THREE: 
+        		* if variable state is equal to 'still' then 
+        			* update the src attribute of this image that you clicked on to what data-animate is equal to for this image
+        			* and update the data-state attribute to 'animate'
+        		* if state does not equal 'still' then 
+        			* update the src attribute of this image that you clicked on to what data-still is equal to for this image
+        			* and update the data-state attribute to 'still'
+			*/
+
+        	//---------------FILL IN CODE HERE FOR STEP THREE----------------------------
             if ( state == 'still'){
                 $(this).attr('src', $(this).data('animate'));
                 $(this).attr('data-state', 'animate');
@@ -95,5 +111,12 @@ $(".animalImage").click(function(){
                 $(this).attr('src', $(this).data('still'));
                 $(this).attr('data-state', 'still');
             }
-});
+            //----------------------------------------------------
+
+            //STEP FOUR: open the file in the browser and click on the images. Then click again to pause.
+	    });
+
+
+
+
 
